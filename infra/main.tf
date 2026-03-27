40c52bf6e379cee0bf04f446064b03d123884343
Ali Riza Keles <113027727+AliRizaKeless@users.noreply.github.com>
2026-03-27 11:48:50 +0100

    Add Azure resources for storage and container

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,23 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
 
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    project = "norway-data-pipeline"
+    owner   = "AliRizaKeless"
+  }
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
