f7cabcee7fce3b12e3e4018baec476d039b13752
Ali Riza Keles <113027727+AliRizaKeless@users.noreply.github.com>
2026-03-27 11:48:30 +0100

    Define Azure resource variables in variables.tf
    Added variables for resource group, location, storage account, and container.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,23 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group name"
+  type        = string
+  default     = "rg-norway-data-pipeline"
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "storage_account_name" {
+  description = "Azure Storage Account name"
+  type        = string
+  default     = "norwaydatapipeline01"
+}
+
+variable "container_name" {
+  description = "Blob container name"
+  type        = string
+  default     = "data"
+}
